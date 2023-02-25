48602a9eb6d9004e80712418869bd9b3494b32f7
Matias Aratano <matiasaratano@gmail.com>
2023-02-25 18:00:56 -0300

    first commit

---
+++ project-carina/src/main/java/com/solvd/carina/demo/api/DeleteUserMethod.java
@@ -0,0 +1,21 @@
+package com.solvd.carina.demo.api;
+
+import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
+import com.qaprosoft.carina.core.foundation.api.annotation.RequestTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.SuccessfulHttpStatus;
+import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
+import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.Configuration;
+
+@Endpoint(url = "${base_url}/users/1", methodType = HttpMethodType.DELETE)
+@RequestTemplatePath(path = "api/users/_delete/rq.json")
+@ResponseTemplatePath(path = "api/users/_delete/rs.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class DeleteUserMethod extends AbstractApiMethodV2 {
+
+    public DeleteUserMethod() {
+        replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
+    }
+}
---
+++ project-carina/src/main/java/com/solvd/carina/demo/api/GetUserMethods.java
@@ -0,0 +1,19 @@
+package com.solvd.carina.demo.api;
+
+import com.qaprosoft.carina.core.foundation.api.AbstractApiMethodV2;
+import com.qaprosoft.carina.core.foundation.api.annotation.Endpoint;
+import com.qaprosoft.carina.core.foundation.api.annotation.ResponseTemplatePath;
+import com.qaprosoft.carina.core.foundation.api.annotation.SuccessfulHttpStatus;
+import com.qaprosoft.carina.core.foundation.api.http.HttpMethodType;
+import com.qaprosoft.carina.core.foundation.api.http.HttpResponseStatusType;
+import com.zebrunner.carina.utils.Configuration;
+
+@Endpoint(url = "${base_url}/users", methodType = HttpMethodType.GET)
+@ResponseTemplatePath(path = "api/users/_get/rs.json")
+@SuccessfulHttpStatus(status = HttpResponseStatusType.OK_200)
+public class GetUserMethods extends AbstractApiMethodV2 {
+
+    public GetUserMethods() {
+        replaceUrlPlaceholder("base_url", Configuration.getEnvArg("api_url"));
+    }
+}
