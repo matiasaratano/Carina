699d10f1f00e02eb866a5b70dc32f7eacb8685e2
Matias Aratano <matiasaratano@gmail.com>
2023-03-14 21:06:01 -0300

    mobile task implementation

---
+++ project-carina/src/test/java/com/solvd/carina/demo/mobile/MobileTaskTest.java
@@ -0,0 +1,9 @@
+package com.solvd.carina.demo.mobile;
+
+import com.qaprosoft.carina.core.foundation.IAbstractTest;
+import com.zebrunner.carina.utils.mobile.IMobileUtils;
+
+public class MobileTaskTest implements IAbstractTest, IMobileUtils {
+
+
+}
